98afae4ebfd9aa2143a0e257d4ac93648a6544c2
shoothzj <shoothzj@gmail.com>
2016-03-01 18:51:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hzj/sh006/NotesDB.java
@@ -0,0 +1,31 @@
+package com.example.hzj.sh006;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by hzj on 16-2-7.
+ */
+public class NotesDB extends SQLiteOpenHelper{
+    public static final String TABLE_NAME = "notes";
+    public static final String CONTENT = "content";
+    public static final String PATH = "path";
+    public static final String VIDEO ="video";
+    public static final String ID = "_id";
+    public static final String TIME = "time";
+    public NotesDB(Context context) {
+        super(context, "notes", null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+TABLE_NAME+" ("+ID
+                +" INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                CONTENT+" TEXT NOT NULL,"+PATH+" TEXT NOT NULL,"+VIDEO+" TEXT NOT NULL,"+TIME+" TEXT NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
